2e405d0087eb349757de218a6d4bff95a553f104
kessily <6kessilly@gmail.com>
2024-11-06 16:12:41 -0300

    Corrigido erro de acesso ao inventoryStatus

--- src/app/demo/components/pages/crud/crud.component.ts
+++ src/app/demo/components/pages/crud/crud.component.ts
@@ -106,7 +106,7 @@ export class CrudComponent implements OnInit {
         if (this.pet.name?.trim()) {
             if (this.pet.id) {
                 // @ts-ignore
-                this.pet.inventoryStatus = this.pet.inventoryStatus.value ? this.pet.inventoryStatus.value : this.pet.inventoryStatus;
+                this.pet.inventoryStatus = this.pet.inventoryStatus ? this.pet.inventoryStatus.value : 'INSTOCK';
                 // this.pets[this.findIndexById(this.pet.id)] = this.pet;
                 this.petService.updatePet(this.pet.key, this.pet);
                 this.messageService.add({ severity: 'success', summary: 'Successful', detail: 'Pet Updated', life: 3000 });
